8f7419b0519b2a86f0de6c18774af5e5564c54bc
Akheedha <akheedhajan20@gmail.com>
2026-04-08 00:12:28 +0530

    done

---
+++ django/day4cw/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def result(request):
+    username = request.GET.get('username')
+    data = request.GET
+
+    return render(request, 'result.html', {
+        'username': username,
+        'data': data
+    })
