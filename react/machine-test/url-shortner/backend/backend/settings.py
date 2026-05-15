5d9f44e54e8cc278f550a4e31f95b9a67ee2f925
akheedha <akheedhajan20@email.com>
2026-05-16 03:27:25 +0530

    finalised

--- react/machine-test/url-shortner/backend/backend/settings.py
+++ react/machine-test/url-shortner/backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'shortner',
+    
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
